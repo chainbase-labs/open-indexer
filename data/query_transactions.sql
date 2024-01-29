b5d52f66fe1ed8afbef5b9ccfe972f27548c45b5
fishTsai20 <123794723+fishTsai20@users.noreply.github.com>
2024-01-30 00:08:59 +0800

    feat[avas]:query transactions from db

---
+++ data/query_transactions.sql
@@ -0,0 +1,66 @@
+select
+    hash,
+    nonce,
+    transaction_index,
+    from_address,
+    to_address,
+    value,
+    gas,
+    gas_price,
+    input,
+    receipt_cumulative_gas_used,
+    receipt_gas_used,
+    receipt_contract_address,
+    receipt_root,
+    receipt_status,
+    block_timestamp,
+    block_number,
+    block_hash,
+    max_fee_per_gas,
+    max_priority_fee_per_gas,
+    transaction_type,
+    receipt_effective_gas_price
+from
+    (
+    (
+    select
+    hash,
+    nonce,
+    transaction_index,
+    from_address,
+    to_address,
+    value,
+    gas,
+    gas_price,
+    input,
+    receipt_cumulative_gas_used,
+    receipt_gas_used,
+    receipt_contract_address,
+    '' AS receipt_root,
+    receipt_status,
+    cast(
+    to_unixtime(block_timestamp) as bigint
+    ) as block_timestamp,
+    block_number,
+    block_hash,
+    max_fee_per_gas,
+    max_priority_fee_per_gas,
+    transaction_type,
+    receipt_effective_gas_price
+    from
+    avalanche.transactions
+    where
+    block_number >= 38780167
+    and block_number <= 40126439
+    ) a
+    join (
+    select
+    tx_hash
+    from
+    avas.raw
+    where
+    block_number >= 38780167
+    and block_number <= 40126439
+    and tick = 'dino'
+    ) b on a.hash = b.tx_hash
+    );
