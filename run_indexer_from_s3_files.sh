959f78c1742b37cbc37f0f4659216c193d494008
fishTsai20 <123794723+fishTsai20@users.noreply.github.com>
2024-01-29 20:01:07 +0800

    feat[avas]:run script

--- run_indexer_from_s3_files.sh
+++ run_indexer_from_s3_files.sh
@@ -10,30 +10,40 @@ LOCAL_DATA_DIR="./data"
 mkdir -p $LOCAL_DATA_DIR
 
 # 获取 S3 桶中的文件列表并按数字排序
-FILES=$(aws s3 ls $S3_BUCKET_PATH | awk '{print $4}' | sort -n)
+FILES=$(aws s3 ls "$S3_BUCKET_PATH/transactions/" | awk '{print $4}' | sort -n)
+echo "$FILES"
 
 # 循环遍历每个文件
 for FILE in $FILES; do
     echo "Processing $FILE..."
 
     # 构建 S3 文件路径和本地文件路径
-    S3_FILE_PATH="$S3_BUCKET_PATH$FILE"
-    LOCAL_FILE_PATH="$LOCAL_DATA_DIR/transactions.txt"
+    S3_TRANSACTION_FILE_PATH="$S3_BUCKET_PATH/transactions/$FILE"
+    S3_LOGS_FILE_PATH="$S3_BUCKET_PATH/logs/$FILE"
+    LOCAL_TRANSACTION_FILE_PATH="$LOCAL_DATA_DIR/transactions.txt"
+    LOCAL_LOG_FILE_PATH="$LOCAL_DATA_DIR/logs.txt"
+
+    echo $S3_TRANSACTION_FILE_PATH
+    echo $S3_LOGS_FILE_PATH
+    echo $LOCAL_TRANSACTION_FILE_PATH
+    echo $LOCAL_LOG_FILE_PATH
 
     # 拉取数据文件
-    aws s3 cp $S3_FILE_PATH $LOCAL_FILE_PATH
+    aws s3 cp $S3_TRANSACTION_FILE_PATH $LOCAL_TRANSACTION_FILE_PATH
+    aws s3 cp $S3_LOGS_FILE_PATH $LOCAL_LOG_FILE_PATH
 
-    echo "download $S3_FILE_PATH ...."
-    if [ -f "$LOCAL_FILE_PATH" ]; then
-        sed -i 's/\"//g' $LOCAL_FILE_PATH
+    if [ -f "$LOCAL_TRANSACTION_FILE_PATH" ] && [ -f "$LOCAL_LOG_FILE_PATH" ]; then
+        sed -i 's/\"//g' $LOCAL_TRANSACTION_FILE_PATH
+        sed -i 's/\"//g' $LOCAL_LOG_FILE_PATH
 
         echo "index avas ...."
-        tidb_db_name=$2 tidb_host=$3 tidb_password=$4 tidb_port=$5 tidb_user=$6 ./indexer --transactions $LOCAL_FILE_PATH --logs ./data/logs.txt
+        tidb_db_name=$2 tidb_host=$3 tidb_password=$4 tidb_port=$5 tidb_user=$6 ./indexer --transactions $LOCAL_TRANSACTION_FILE_PATH --logs $LOCAL_LOG_FILE_PATH
     else
         echo "Failed to download $FILE"
     fi
 
-    rm $LOCAL_FILE_PATH
+    rm $LOCAL_TRANSACTION_FILE_PATH
+    rm $LOCAL_LOG_FILE_PATH
 done
 
 echo "All files processed."
