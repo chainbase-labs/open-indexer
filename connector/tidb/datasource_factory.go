a62f67c7bc50b329118405e77de350797ac13986
fishTsai20 <123794723+fishTsai20@users.noreply.github.com>
2024-01-29 20:13:43 +0800

    chore[avas]:change db log level to info

--- connector/tidb/datasource_factory.go
+++ connector/tidb/datasource_factory.go
@@ -58,7 +58,7 @@ func createDB(tidb_user string, tidb_password string, tidb_host string, tidb_por
 		tidb_user, tidb_password, tidb_host, tidb_port, tidb_db_name)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Error),
+		Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
